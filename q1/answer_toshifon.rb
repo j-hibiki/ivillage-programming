37665c0d819753b6ab2cbd398dcc5713141d7060
Toshinori Nishimura <wasedaboy@msn.com>
2015-03-01 19:19:48 +0900

    Did it!

--- q1/answer_toshifon.rb
+++ q1/answer_toshifon.rb
@@ -1,1 +1,21 @@
-test
+BRANCH  = "|-- "
+VBRANCH = "| "
+EBRANCH = "└ "
+SPACE   = "  "
+
+children = Dir.glob("#{ARGV[0]}/*")
+
+puts ARGV[0]
+
+for child in children do
+  puts child.gsub(ARGV[0] + "/",BRANCH)
+  grandChildren = Dir.glob("#{child}/*")
+
+  for grandChild in grandChildren do
+    if child == children[children.length-1] 
+      puts grandChild.gsub(child+"/", " " + SPACE + BRANCH)
+    else
+      puts grandChild.gsub(child+"/", VBRANCH + SPACE + BRANCH)
+    end
+  end
+end
