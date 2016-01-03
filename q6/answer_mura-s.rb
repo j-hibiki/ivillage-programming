a1bd53a7c7d48837e9d5bd88c1d59b0305c970fa
mura-s <seiya.muramatsu1@gmail.com>
2016-01-03 23:16:16 +0900

    mura-sのq6の回答を追加

---
+++ q6/answer_mura-s.rb
@@ -0,0 +1,57 @@
+# 実行方法:
+#   ruby answer_mura-s.rb 1224
+
+ANS = 10.0
+OPS = ['+', '-', '*', '/']
+
+def patterns(n1, n2, n3, n4, op1, op2, op3)
+	[
+    "#{n1} #{op1} #{n2} #{op2} #{n3} #{op3} #{n4}",
+    "(#{n1} #{op1} #{n2}) #{op2} #{n3} #{op3} #{n4}",
+    "#{n1} #{op1} (#{n2} #{op2} #{n3}) #{op3} #{n4}",
+    "#{n1} #{op1} #{n2} #{op2} (#{n3} #{op3} #{n4})",
+    "(#{n1} #{op1} #{n2} #{op2} #{n3}) #{op3} #{n4}",
+    "#{n1} #{op1} (#{n2} #{op2} #{n3} #{op3} #{n4})",
+    "(#{n1} #{op1} #{n2} #{op2} #{n3} #{op3} #{n4})",
+    "(#{n1} #{op1} #{n2}) #{op2} (#{n3} #{op3} #{n4})",
+    "((#{n1} #{op1} #{n2}) #{op2} #{n3}) #{op3} #{n4}",
+    "(#{n1} #{op1} (#{n2} #{op2} #{n3})) #{op3} #{n4}",
+    "#{n1} #{op1} ((#{n2} #{op2} #{n3}) #{op3} #{n4})",
+    "#{n1} #{op1} (#{n2} #{op2} (#{n3} #{op3} #{n4}))",
+    "((#{n1} #{op1} #{n2}) #{op2} #{n3} #{op3} #{n4})",
+    "(#{n1} #{op1} (#{n2} #{op2} #{n3}) #{op3} #{n4})",
+    "(#{n1} #{op1} #{n2} #{op2} (#{n3} #{op3} #{n4}))",
+    "((#{n1} #{op1} #{n2} #{op2} #{n3}) #{op3} #{n4})",
+    "(#{n1} #{op1} (#{n2} #{op2} #{n3} #{op3} #{n4}))",
+    "((#{n1} #{op1} #{n2}) #{op2} (#{n3} #{op3} #{n4}))",
+    "(((#{n1} #{op1} #{n2}) #{op2} #{n3}) #{op3} #{n4})",
+    "((#{n1} #{op1} (#{n2} #{op2} #{n3})) #{op3} #{n4})",
+    "(#{n1} #{op1} ((#{n2} #{op2} #{n3}) #{op3} #{n4}))",
+    "(#{n1} #{op1} (#{n2} #{op2} (#{n3} #{op3} #{n4})))"
+  ]
+end
+
+def evalEachPatterns(n1, n2, n3, n4, op1, op2, op3)
+  pats = []
+  patterns(n1, n2, n3, n4, op1, op2, op3).each do |pat|
+    pats << pat if eval(pat) == ANS
+  end
+  pats
+end
+
+def printResultArray(pats)
+  pats.each { |pat| puts pat }
+end
+
+def main(num)
+  n1, n2, n3, n4 = num.split('').map(&:to_f)
+  for op1 in OPS do
+    for op2 in OPS do
+      for op3 in OPS do
+        printResultArray(evalEachPatterns(n1, n2, n3, n4, op1, op2, op3))
+      end
+    end
+  end
+end
+
+main(ARGV[0])
